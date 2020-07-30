8ae7fa06a5cf004c5ada872f4ca16258d56aa5b4
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-07-30 16:39:30 -0400

    done

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,41 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart.each do |item_hash|
+    coupons.each do |coupon_item_hash| 
+      if  item_hash[:item] == coupon_item_hash[:item] && item_hash[:count] >= coupon_item_hash[:num]
+          item_hash[:count] -= coupon_item_hash[:num]
+          coupon_cart = {}
+          coupon_cart[:item] = "#{item_hash[:item]} W/COUPON"
+          coupon_cart[:price] = coupon_item_hash[:cost] / coupon_item_hash[:num]
+          coupon_cart[:clearance] = item_hash[:clearance]
+          coupon_cart[:count] = coupon_item_hash[:num]
+          cart.push(coupon_cart)
+          # binding.pry
+      end
+    end
+  end
+  cart
 end
+
+# item_hash[:item] == coupon_item_hash[:item] && item_hash[:count] >= coupon_item_hash[:num]
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart.each do |item_hash|
+    if item_hash[:clearance]
+      discount = item_hash[:price] * 0.20
+      item_hash[:price] -= discount
+      # binding.pry
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
@@ -22,4 +48,23 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidate_cart = consolidate_cart(cart)
+  consolidate_cart = apply_coupons(consolidate_cart, coupons)
+  consolidate_cart = apply_clearance(consolidate_cart)
+
+  total = 0
+  consolidate_cart.each do |item_hash|
+    total += item_hash[:price] * item_hash[:count]
+    # binding.pry
+  end
+
+  if total > 100
+    discount = total * 0.10
+    total -= discount
+  end
+
+  total
+
+
+  # binding.pry
 end
--- lib/part_1_solution.rb
+++ lib/part_1_solution.rb
@@ -1,34 +1,51 @@
+# def find_item_by_name_in_collection(name, collection)
+#   index = 0
+
+#   collection.each do |grocery_item|
+#     return grocery_item if grocery_item[:item] === name 
+#     index += 1
+#   end
+
+#   nil
+# end
+
+
+# def consolidate_cart(cart)
+#   index = 0
+#   new_cart = []
+  
+#   cart.each do |grocery_item|
+#     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+#     if current_item
+#       new_cart_index = 0
+#       new_cart.each do |new_cart_item|
+#         if new_cart_item[:item] === current_item[:item]
+#           new_cart_item[:count] += 1
+#         end
+#         new_cart_index += 1
+#       end
+#     else
+#       grocery_item[:count] = 1
+#       new_cart << grocery_item
+#     end
+#     index += 1
+#   end
+#   new_cart
+# end
+
 def find_item_by_name_in_collection(name, collection)
-  index = 0
-
-  collection.each do |grocery_item|
-    return grocery_item if grocery_item[:item] === name 
-    index += 1
+  collection.each do |item_info|
+    item_info.each{|key, value| return item_info if value == name}
+  # binding.pry
   end
-
   nil
 end
 
-
 def consolidate_cart(cart)
-  index = 0
-  new_cart = []
-  
-  cart.each do |grocery_item|
-    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
-    if current_item
-      new_cart_index = 0
-      new_cart.each do |new_cart_item|
-        if new_cart_item[:item] === current_item[:item]
-          new_cart_item[:count] += 1
-        end
-        new_cart_index += 1
-      end
-    else
-      grocery_item[:count] = 1
-      new_cart << grocery_item
-    end
-    index += 1
+  single = cart.uniq
+  single.each do|info_hash| 
+    info_hash[:count] = cart.count{|x| x == info_hash} 
+    # binding.pry
   end
-  new_cart
 end
+
